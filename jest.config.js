83acc522fb5ae135030fb1f822f8a2573b6bec2f
Dylan Green <dylan.green61@gmail.com>
2024-12-16 14:09:20 -0700

    foundations; .gitignore, .npmignore, configs and package.json configured

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    'node_modules/variables/.+\\.(j|t)sx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['node_modules/(?!variables/.*)'],
+  roots: ['<rootDir>'],
+  modulePaths: ['<rootDir>'],
+  moduleDirectories: ['node_modules', 'src'],
+};
