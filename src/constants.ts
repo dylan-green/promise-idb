cb2c3a80768bef9fa8d77a068526bc83818b2a52
Dylan Green <dylan.green61@gmail.com>
2024-12-16 23:17:04 -0700

    add transaction mode constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,7 @@
 export const INDEXED_DB: string = 'indexedDB';
 
 export const NO_INDEXED_DB: string = 'window or window.indexedDB is undefined';
+
+export const READ_ONLY: IDBTransactionMode = 'readonly';
+
+export const READ_WRITE: IDBTransactionMode = 'readwrite';
